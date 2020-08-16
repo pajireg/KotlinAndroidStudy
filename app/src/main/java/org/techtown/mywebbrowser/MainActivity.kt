e7cb4dbe9db88b8bcae197f24e79aa99b1090c7b
choisumin <choisumin@valti.co.kr>
2020-08-16 17:39:02 +0900

    Initial commit

---
+++ app/src/main/java/org/techtown/mywebbrowser/MainActivity.kt
@@ -0,0 +1,114 @@
+package org.techtown.mywebbrowser
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.ContextMenu
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.webkit.WebViewClient
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.browse
+import org.jetbrains.anko.email
+import org.jetbrains.anko.sendSMS
+import org.jetbrains.anko.share
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // 웹뷰 기본 설정
+        webView.apply {
+            settings.javaScriptEnabled = true // 자바스크립트 기능 동작
+            webViewClient = WebViewClient() // 자체브라우저가 아닌 웹뷰에서 페이지를 로딩
+        }
+        webView.loadUrl("https://www.google.com")
+
+        urlEditTest.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                webView.loadUrl(urlEditTest.text.toString())
+                true
+            } else {
+                false
+            }
+        }
+
+        // 컨텍스트 메뉴 등록
+        registerForContextMenu(webView)
+    }
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item?.itemId) {
+            R.id.action_google, R.id.action_home -> {
+                webView.loadUrl("https://www.google.com")
+                return true
+            }
+            R.id.action_naver -> {
+                webView.loadUrl("https://www.naver.com")
+                return true
+            }
+            R.id.action_daum -> {
+                webView.loadUrl("https://www.daum.net")
+                return true
+            }
+            R.id.action_call -> {
+                val intent = Intent(Intent.ACTION_DIAL)
+                intent.data = Uri.parse("tel:052-123-4567")
+                if (intent.resolveActivity(packageManager) != null) {
+                    startActivity(intent)
+                }
+                return true
+            }
+            R.id.action_send_text -> {
+                // 문자 보내기
+                sendSMS("052-123-4567", webView.url)
+                return true
+            }
+            R.id.action_email -> {
+                // 이메일 보내기
+                email("test@email.com", "이메일 제목", webView.url)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        menuInflater.inflate(R.menu.context, menu)
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        when (item?.itemId) {
+            R.id.action_share -> {
+                // 페이지 공유
+                share(webView.url)
+                return true
+            }
+            R.id.action_browser -> {
+                browse(webView.url)
+                return true
+            }
+        }
+        return super.onContextItemSelected(item)
+    }
+}
